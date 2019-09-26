265fd76c0596c5fa9eba5dd8000f2d02eefeaade
mirzasb <mirzasbaig@nyu.edu>
2019-09-26 17:44:19 -0400

    Section 3 and 4

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     {/* <SEO title="Home" /> */}
     <div className="Hero">
         <div className="HeroGroup">
-        <h1>Learn to design and code React Apps</h1>
+        <h1>Learn to <br />design and code React Apps</h1>
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
         <Link to="/page-2/">Watch the video</Link>
       </div>
