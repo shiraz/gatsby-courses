ac5ea64e94ac24b76f83ea348edd7d9a37a4dd81
mirzasb <mirzasbaig@nyu.edu>
2019-09-30 18:00:04 -0400

    Completed tutorial 7 and 8

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -4,24 +4,47 @@ import React from "react"
 
 import './Header.css';
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../../static/images/logo-designcode.svg')} width="30" /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  constructor(props) {
+    super(props);
 
-Header.defaultProps = {
-  siteTitle: ``,
+    this.state = {
+      hasScrolled: false
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50) {
+      this.setState({
+        hasScrolled: true
+      });
+    } else {
+      this.setState({
+        hasScrolled: false
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../../static/images/logo-designcode.svg')} width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header
