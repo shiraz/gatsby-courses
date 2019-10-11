18cc9f8ec7a34e20cf2f85b66f394df832fb3c98
mirzasb <mirzasbaig@nyu.edu>
2019-10-10 20:12:43 -0400

    Section 20

--- tutorial/src/components/layout.js
+++ tutorial/src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './layout.css'
 
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -6,5 +6,12 @@ export default () => (
     <h1 className="title">This is our home page</h1>
     <h1 className="title">another heading</h1>
     <h2>The second heading</h2>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">hello</div>
+        <div className="col-4">hello</div>
+        <div className="col-4">hello</div>
+      </div>
+    </div>
   </Layout>
 )
