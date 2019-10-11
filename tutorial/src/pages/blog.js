a94e80991891c6f49611661554af5f6d9cc00d3c
mirzasb <mirzasbaig@nyu.edu>
2019-10-10 20:03:12 -0400

    Tutorial 19

--- tutorial/src/pages/blog.js
+++ tutorial/src/pages/blog.js
@@ -2,12 +2,17 @@ import React, { Component } from "react"
 import { Link } from "gatsby"
 import Layout from '../components/layout'
 
-import '../components/blog.css'
+import styles from '../components/blog.module.css'
 
 export default class blog extends Component {
   render() {
     return (
-      <Layout><h1 className="blog-title">This is our blog page</h1></Layout>
+      <Layout>
+        <div className={styles.blog}>
+          <h1 className={styles.title}>This is our blog page</h1>
+          <h2>this second heading</h2>
+        </div>
+      </Layout>
     )
   }
 }
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -3,6 +3,8 @@ import Layout from '../components/layout'
 
 export default () => (
   <Layout>
-    <h1>This is our home page</h1>
+    <h1 className="title">This is our home page</h1>
+    <h1 className="title">another heading</h1>
+    <h2>The second heading</h2>
   </Layout>
 )
