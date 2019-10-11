a84135d7dc1e31156264fb7079301c200ca8ad68
mirzasb <mirzasbaig@nyu.edu>
2019-10-11 18:23:15 -0400

    Update gatsby config

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -5,5 +5,10 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'tutorial',
+    description: 'just some description about our site',
+    author: '@johndoe'
+  },
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`]
 }
