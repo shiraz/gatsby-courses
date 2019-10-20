a6851c3ea061cc0646e87e99ad48f0a4c1f2278b
mirzasb <mirzasbaig@nyu.edu>
2019-10-20 00:16:48 -0400

    Everything until tutorial #44

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -14,5 +14,18 @@ module.exports = {
       age: 25
     }
   },
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`]
+  plugins: [
+     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      }
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+
+    ]
 }
