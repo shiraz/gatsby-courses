9682fda973c7ea6ced23ba818567ad0a64887d3f
mirzasb <mirzasbaig@nyu.edu>
2019-11-07 18:54:21 -0500

    Fix layout

--- backroads/src/components/Layout.js
+++ backroads/src/components/Layout.js
@@ -1,15 +1,18 @@
-import React from "react"
-import Navbar from "./Navbar"
-import Footer from "./Footer"
-import "./layout.css"
-const Layout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      {children}
-      <Footer />
-    </>
-  )
+import React from 'react'
+
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+import styles from './layout.css'
+
+const Layout = ( {children} ) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+    )
 }
 
 export default Layout
