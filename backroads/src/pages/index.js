d7a7840983b9bcca5d2e9b4d5e528e1b7c18487e
mirzasb <mirzasbaig@nyu.edu>
2019-11-20 22:45:45 -0500

    Fix index.js page

--- backroads/src/pages/index.js
+++ backroads/src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-import Layout from "../components/Layout"
 import { FaAdobe } from "react-icons/fa"
+
+import Layout from "../components/Layout"
+
 export default () => (
   <Layout>
     <div>hello from backroads</div>
