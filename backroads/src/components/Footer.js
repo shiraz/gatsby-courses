71fc355bd21c4f764f91befcd1d0513dde2ebfe7
mirzasb <mirzasbaig@nyu.edu>
2019-11-22 17:23:34 -0500

    Setup Footer

--- backroads/src/components/Footer.js
+++ backroads/src/components/Footer.js
@@ -1,9 +1,42 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
+
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
+
+import styles from '../css/footer.module.css'
+
 const Footer = () => {
   return (
-    <div>
-      <h1>I'm footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link key={index} to={item.path}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear()} all
+        rights reserved
+      </div>
+    </footer>
   )
 }
 
