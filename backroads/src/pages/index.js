154f80807a0494cb86094950c70337a333b95d94
mirzasb <mirzasbaig@nyu.edu>
2019-11-22 17:37:37 -0500

    Setup Banner

--- backroads/src/pages/index.js
+++ backroads/src/pages/index.js
@@ -1,11 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
-import { FaAdobe } from "react-icons/fa"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/Layout"
+import Layout from '../components/Layout'
+import Banner from '../components/Banner'
+import SimpleHero from '../components/SimpleHero'
 
 export default () => (
   <Layout>
-    <div>hello from backroads</div>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
