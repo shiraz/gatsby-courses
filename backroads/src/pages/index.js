0cdc02fb733d54c63658bd87d81fedbc7e583669
mirzasb <mirzasbaig@nyu.edu>
2019-12-12 16:39:54 -0500

    Implemented services

--- backroads/src/pages/index.js
+++ backroads/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/Layout'
-import SimleHero from '../components/SimpleHero'
-import Banner from '../components/Banner'
 import About from '../components/Home/About'
+import Banner from '../components/Banner'
+import Layout from '../components/Layout'
+import Services from '../components/Home/Services'
+import SimleHero from '../components/SimpleHero'
 
 export default () => (
   <Layout>
@@ -19,5 +20,6 @@ export default () => (
       </Banner>
     </SimleHero>
     <About />
+    <Services />
   </Layout>
 )
