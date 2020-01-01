16afb2005506cb93690088017308c9cfd7031876
mirzasb <mirzasbaig@nyu.edu>
2019-12-31 21:15:07 -0500

    Add About background image

--- backroads/src/components/Home/About.js
+++ backroads/src/components/Home/About.js
@@ -1,38 +1,51 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Title from '../Title'
 
 import styles from '../../css/about.module.css'
 
-import img from '../../images/defaultBcg.jpeg'
+const getAboutImg = graphql`
+  query aboutImage {
+    aboutImage:file(relativePath:{eq:"defaultBcg.jpeg"}) {
+      childImageSharp {
+        fluid(maxWidth:600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 const About = () => {
-    return (
-      <section className={styles.about}>
-        <Title title="about" subtitle="us" />
-        <div className={styles.aboutCenter}>
-          <article className={styles.aboutImg}>
-            <div className={styles.imgContainer}>
-              <img src={img} alt="about company" />
-            </div>
-          </article>
-          <article className={styles.aboutInfo}>
-            <h4>explore the difference</h4>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
-              doloribus enim necessitatibus?
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
-              doloribus enim necessitatibus?
-            </p>
-            <button type="button" className="btn-primary">
-              read more
-            </button>
-          </article>
-        </div>
-      </section>
-    )
-  }
+  const { aboutImage } = useStaticQuery(getAboutImg)
+  return (
+    <section className={styles.about}>
+      <Title title="about" subtitle="us" />
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <Img fluid={aboutImage.childImageSharp.fluid} alt="awesome landscape" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            doloribus enim necessitatibus?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            doloribus enim necessitatibus?
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
+  )
+}
 
 export default About
--- backroads/src/pages/blog.js
+++ backroads/src/pages/blog.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Images from '../examples/Images'
 
 const blog = () => {
   return (
     <Layout>
       hello from blog page<Link to='/'>back home</Link>
-      <Images />
     </Layout>
   )
 }
