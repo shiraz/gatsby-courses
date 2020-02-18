97ac268e6879693a100a7bfe161007f007fdebfe
mirzasb <mirzasbaig@nyu.edu>
2020-02-18 18:20:04 -0500

    Setup contentful

--- coffee/gatsby-config.js
+++ coffee/gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/logo.svg`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.COFFEE_SPACE_ID,
+        accessToken: process.env.COFFEE_CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
